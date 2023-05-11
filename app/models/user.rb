9d978180815c12dc31e22184ba041deca039a02f
Brandon Castro <castroyou999@gmail.com>
2023-05-10 23:14:17 -0400

    switch service to s3 for user image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_one_attached :avatar, dependent: :destroy
+    # has_one_attached :avatar, dependent: :destroy
+    has_one_attached :avatar, service: :s3, dependent: :destroy
 
 
 end
